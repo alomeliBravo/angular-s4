770b1154b72fa056812d18ac0d1b566a114afc7d
Angel Lomeli <alomelibravo@gmail.com>
2024-12-13 22:24:40 -0700

    Video #46 One way data binding

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -12,5 +12,16 @@ export class HeroComponent {
   public name:string = 'ironman';
   public age:number = 45;
 
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string{
+    return `${this.name} - ${this.age}`;
+  }
+
+  get capitalizedName():string {
+
+  }
 
 }
