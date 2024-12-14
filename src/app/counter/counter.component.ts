b7ac8cc94c5bf824a0226dda7bbe85227b322b8f
Angel Lomeli <alomelibravo@gmail.com>
2024-12-13 21:40:45 -0700

    Video #43

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,10 +1,26 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   standalone: false,
   selector: 'app-counter',
-  template: `<h1>Fokin standalone</h1>`,
+  template: `
+    <h3>counter: {{counter}}</h3>
+    <button (click)="increaseBy(+1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `
 })
-export class CounterComponent {
 
+export class CounterComponent {
+  constructor() { }
+
+  public counter:number = 9;
+
+  increaseBy(value:number):void {
+    this.counter += value;
+  }
+
+  resetCounter(){
+    this.counter = 10;
+  }
 }
