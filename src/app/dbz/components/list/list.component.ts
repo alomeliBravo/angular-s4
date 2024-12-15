97b4d6e06d072b51f9f5d81666299eb8b55c9ffb
Angel Lomeli <alomelibravo@gmail.com>
2024-12-15 16:57:28 -0700

    Tarea #62 Crear componente add-character

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent { }
