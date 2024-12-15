69cb57441f5774fbc46560b0c29a2331eafda92b
Angel Lomeli <alomelibravo@gmail.com>
2024-12-15 15:51:49 -0700

    Videos #58 y 59

---
+++ src/app/dbz/components/pages/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component} from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+  constructor() { }
+
+}
