2b106bc5e674cd13c27fed4c500f486a34a5f3e6
Angel Lomeli <alomelibravo@gmail.com>
2024-12-17 18:07:28 -0700

    Video #72 y 73 Tarea refactorizar metodo deleteByID

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({providedIn: 'root'})
 export class DbzService {
   constructor() { }
 
   public characters:Character[] = [{
+    id: uuid(),
     name: 'Krilin',
     power: 1000
   },{
+    id: uuid(),
     name: 'Goku',
     power: 9500
   },{
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
 
   onNewCharacter( character:Character ):void {
-    this.characters.push(character);
+
+    const newCharacter: Character = { id: uuid(), ...character}
+
+    this.characters.push(newCharacter);
   }
 
-  onDeleteId( index:number ): void{
-    this.characters.splice(index,1);
+  // onDeleteId( index:number ): void{
+  //   this.characters.splice(index,1);
+  // }
+
+  deleteCharacterById( id:string ) {
+    this.characters = this.characters.filter( character => character.id !== id );
   }
 }
