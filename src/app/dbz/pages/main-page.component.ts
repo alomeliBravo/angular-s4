76c61bda66b3516103d37befb4389cd29fcd8fff
Angel Lomeli <alomelibravo@gmail.com>
2024-12-17 18:21:02 -0700

    Video #74 Servicio privado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   standalone: false,
@@ -8,8 +9,17 @@ import { DbzService } from '../services/dbz.service';
 })
 
 export class MainPageComponent {
-  constructor(public dbzService:DbzService ) {
+  constructor( private dbzService:DbzService ) {}
 
+  get characters(): Character[] {
+    return [ ...this.dbzService.characters ];
   }
 
+  onDeleteCharacter( id:string ):void {
+    this.dbzService.deleteCharacterById ( id );
+  }
+
+  onNewCharacter( character: Character ) {
+    this.dbzService.addCharacter(character);
+  }
 }
